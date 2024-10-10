ed1e63ba5ced27929eb9fd50584cac449bb95852
sinshisko <sesrvkis@gmail.com>
2024-10-10 09:43:18 +0000

    updated ps4 map

--- src/ps4_controller/ps4_commons.py
+++ src/ps4_controller/ps4_commons.py
@@ -1,37 +1,40 @@
 import pygame
 
+BUTTON_MIN_MAX_RELEASED_MAP = {"min":0,"max":1,"released":0}
+
+
 ps4_buttons = {
-    0: "Square",
-    1: "X",
-    2: "Circle",
-    3: "Triangle",
+    0: {"name":"Square"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    1: {"name":"Cross"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    2: {"name":"Circle"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    3: {"name":"Triangle"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
     # D-pad
-    11: "Up",
-    12: "Down",
-    13: "Left",
-    14: "Right",
+    11: {"name":"Up"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    12: {"name":"Down"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    13: {"name":"Left"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    14: {"name":"Right"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
     # L1, R1, L2, R2
-    4: "L1",
-    5: "R1",
-    6: "L2",
-    7: "R2",
+    4: {"name":"L1"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    5: {"name":"R1"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    6: {"name":"L2"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    7: {"name":"R2"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
     # joystick buttons
-    10: "L3",
-    11: "R3",
+    10: {"name":"L3"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    11: {"name":"R3"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
     # Share, Options, PS
-    8: "Share",
-    9: "Options",
-    12: "PS",
+    8: {"name":"Share"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    9: {"name":"Options"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
+    12: {"name":"PS"}.update(*BUTTON_MIN_MAX_RELEASED_MAP),
     
 }
 
 ps4_axis = {
-    0: "Left Stick X",
-    1: "Left Stick Y",
-    2: "Right Stick X",
-    5: "Right Stick Y",
-    3: "L2", # -1 Neutral , 1 Fully pressed
-    4: "R2", # -1 Neutral , 1 Fully pressed
+    0: {"name":"Left Stick X","min":-1,"max":1,"released":0},
+    1: {"name":"Left Stick Y","min":-1,"max":1,"released":0},
+    2: {"name":"Right Stick X","min":-1,"max":1,"released":0},
+    5: {"name":"Right Stick Y","min":-1,"max":1,"released":0},
+    3: {"name":"L2","min":-1,"max":1,"released":-1},
+    4: {"name":"R2","min":-1,"max":1,"released":-1},
 }
 
 
